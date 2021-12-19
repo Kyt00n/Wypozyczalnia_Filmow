c7214bb0cadea564c6f5312bc39b0f08ddfd794e
swiderekm <79854725+swiderekm@users.noreply.github.com>
2021-12-19 22:57:59 +0100

    Add files via upload
    początek

---
+++ projekcik/projekcik/Klient.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace projekcik
+{
+    class Klient : Osoba
+    {
+        string idKlienta;
+        public static int licznik_klientow = 0;
+        int do_Zaplaty;
+
+        public Klient(string imie, string nazwisko, string pesel, int numerTelefonu, int wiek, string email, string idKlienta, int do_Zaplaty) : base(imie, nazwisko, pesel, numerTelefonu, wiek, email)
+        {
+            this.idKlienta = idKlienta;
+            this.do_Zaplaty = do_Zaplaty;
+            Interlocked.Increment(ref licznik_klientow);
+        }
+        
+        public override string ToString()
+        {
+            return base.ToString() + ", ID klienta: " + idKlienta + ", należności: " + do_Zaplaty + "zł";
+        }
+    }
+}
---
+++ projekcik/projekcik/Wypozyczalnia.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projekcik
+{
+    class Wypozyczalnia
+    {
+        List<Wypozyczenie> wypozyczenia;
+        List<Pracownik> pracownicy;
+        string nazwa;
+        double oplata_podstawowa;
+        double oplata_dodatkowa;
+
+
+    }
+}
